42797f2ef6a74e3961a385485ed98f84205c74ca
andakagdv <andaka@goodeva.co.id>
2024-02-12 15:45:38 +0700

    add seeder superadmin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            // $this->call(CatatanKeuanganSeeder::class)
+            $this->call(UserSeeder::class)
         ]);
     }
 }
