970afaf1769daebc50135f6ccd1c3e192d4040c6
adl.tomie <adl.tomie@xl.co.id>
2025-01-07 08:55:57 +0700

    fixing head

--- resources/views/layouts-fe/partials/head.blade.php
+++ resources/views/layouts-fe/partials/head.blade.php
@@ -1,15 +1,43 @@
 <meta charset="UTF-8">
-<meta name="keywords" content="HVAC, unica, creative, html">
+<meta name="keywords" content="Hino Bogor, Dealer Hino, Truk Hino, Bus Hino, Dealer Resmi Hino, Jual Truk Bogor">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>HINO</title>
+<title>HINO Bogor - Dealer Resmi Hino di Bogor</title>
+<meta name="description" content="Dealer resmi Hino di Bogor. Dapatkan truk dan bus Hino dengan penawaran terbaik. Hubungi kami untuk konsultasi dan penawaran spesial!">
+
+<script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "LocalBusiness",
+      "name": "HINO Bogor",
+      "description": "Dealer resmi Hino di Bogor menyediakan berbagai model truk dan bus dengan layanan terbaik.",
+      "image": "https://hino-bogor.com/assets/img/thumbnail/thumbnail-hino.png",
+      "url": "https://hino-bogor.com",
+      "telephone": "+62-123-456-789",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Jalan Raya Bogor No. 123",
+        "addressLocality": "Bogor",
+        "addressRegion": "Jawa Barat",
+        "postalCode": "16100",
+        "addressCountry": "ID"
+      },
+      "openingHours": "Mo-Fr 08:00-17:00",
+      "sameAs": [
+        "https://www.facebook.com/hinobogor",
+        "https://www.instagram.com/hinobogor"
+      ]
+    }
+    </script>
+
+
 
 <!-- Open Graph Meta Tags -->
-<meta property="og:title" content="HINO Jakarta Indonesia">
-{{-- <meta property="og:description" content="HVAC Template"> --}}
+<meta property="og:title" content="HINO Bogor - Dealer Resmi Hino">
+<meta property="og:description" content="Dealer resmi Hino di Bogor menyediakan berbagai model truk dan bus dengan layanan terbaik. Hubungi kami sekarang!">
 <meta property="og:image:secure"content="{{ asset('assets/img/thumbnail/thumbnail-hino.png') }}">
 <meta property="og:image" content="{{ asset('assets/img/thumbnail/thumbnail-hino.png') }}">
-<meta property="og:url" content="https://hinojakartaindonesia.com">
+<meta property="og:url" content="https://hino-bogor.com">
 <meta property="og:type" content="website">
 
 <!-- Twitter Card Meta Tags -->
@@ -18,6 +46,7 @@
 {{-- <meta name="twitter:description" content="HVAC Template"> --}}
 <meta name="twitter:image" content="{{ asset('assets/img/thumbnail/thumbnail-hino.png') }}">
 
+<link rel="canonical" href="https://hino-bogor.com">
 <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('assets/img/thumbnail/thumbnail-hino.png') }}" />
 <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/img/thumbnail/thumbnail-hino.png') }}" />
 <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/thumbnail/thumbnail-hino.png') }}" />
